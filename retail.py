f7322f1ad82468a9e955d251e015960b18a3012f
Alejandra Trejo <alejandratrdz@gmail.com>
2019-05-14 23:44:28 +0400

    print and list items

--- retail.py
+++ retail.py
@@ -14,6 +14,15 @@ class Store:
         else:
             self.non_grocery.append(Item(item_name, item_price))
 
+    def list_items(self):
+        for product in self.grocery_items:
+            print(product)
+        for product in self.non_grocery:
+            print(product)
+
+
+
+
 class Item:
     def __init__(self, name, price):
         self.name = name
@@ -22,9 +31,28 @@ class Item:
     def get_price(self):
         return self.price
 
+    def __str__(self):
+        return "{}\t{}".format(self.name, self.price)
+
 
 def main():
-    pass
+    # Set up store
+    mck_store = Store("McKinsey Store")
+
+    # Add items to store
+    mck_store.add_item("Mandarins", 4.5, "grocery")
+    mck_store.add_item("Apples", 2.0, "grocery")
+    mck_store.add_item("Cheese", 5.5, "grocery")
+    mck_store.add_item("Chocolates", 10.75, "grocery")
+    mck_store.add_item("Salt", 1.0, "grocery")
+    mck_store.add_item("Detergent", 22.5, "non-grocery")
+    mck_store.add_item("Toilet paper", 16.25, "non-grocery")
+    mck_store.add_item("Pen", 1.0, "non-grocery")
+    mck_store.add_item("Notebook", 5.0, "non-grocery")
+    mck_store.add_item("Mop", 34.5, "non-grocery")
+
+    # List items
+    mck_store.list_items()
 
 
 if __name__ == '__main__':
